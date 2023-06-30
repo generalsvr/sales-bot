6000bf2566494c4382a4f66c2eb827f000ee9007
_kernelpanic <kernel@pan.ic>
2023-06-30 11:42:57 +0300

    0.1

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher.filters import Command
 from aiogram.utils import executor
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-from prompts import SYSTEM_PROMPT
+from prompts import SYSTEM_PROMPT, INIT_MSG
 from embeddings import search
 from langchain.prompts import (
     ChatPromptTemplate,
@@ -17,7 +17,7 @@ from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 
-bot = Bot(token="5828347904:AAFP4uv7X1mkRWC4bamYL-ezGQswnyO2hNk")
+bot = Bot(token="6321687305:AAGQRd_nlp6CFO44gaq_xrqptWSqtdyW040")
 dp = Dispatcher(bot, storage=MemoryStorage())
 MODEL = "gpt-4" # gpt-3.5-turbo 
 
@@ -26,28 +26,49 @@ class StateMachine(StatesGroup):
     CHAT = State()
 
 @dp.message_handler(Command('agents'), state="*")
-async def agents_handler(message: types.Message):
-    agents = ["Agent 1", "Agent 2", "Agent 3"]
-    keyboard = types.InlineKeyboardMarkup()
+async def agents_handler(message: types.Message, state: FSMContext):
+    agents = ["Basic", "Advanced"]
+    keyboard = types.InlineKeyboardMarkup(resize_keyboard=True, row_width=1)
     buttons = [types.InlineKeyboardButton(agent, callback_data=agent) for agent in agents]
     keyboard.add(*buttons)
-    await bot.send_message(message.chat.id, "Choose an agent:", reply_markup=keyboard)
 
-@dp.message_handler(Command('settings'), state="*")
-async def settings_handler(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup()
+    data = await state.get_data()
+    lang = data.get("language", "english")
+
+    if lang == "english":
+        await bot.send_message(message.chat.id, "🤖 Choose an agent:", reply_markup=keyboard)
+    elif lang == "russian":
+        await bot.send_message(message.chat.id, "🤖 Выберите агента:", reply_markup=keyboard)
+
+@dp.message_handler(Command('language'), state="*")
+async def settings_handler(message: types.Message, state: FSMContext):
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
     buttons = [
-        types.InlineKeyboardButton("RU", callback_data="russian"),
-        types.InlineKeyboardButton("EN", callback_data="english")
+        types.InlineKeyboardButton("🇷🇺 RU", callback_data="russian"),
+        types.InlineKeyboardButton("🇺🇸 EN", callback_data="english")
     ]
     keyboard.add(*buttons)
-    await message.answer("Choose a language:", reply_markup=keyboard)
+
+    data = await state.get_data()
+    lang = data.get("language", "english")
+
+    if lang == "english":
+        await message.answer("Choose a language:", reply_markup=keyboard)
+    elif lang == "russian":
+        await message.answer("Выберите язык:", reply_markup=keyboard)
 
 @dp.message_handler(Command('start'), state="*")
 async def start_command(message: types.Message, state: FSMContext):
     await StateMachine.MAIN_MENU.set()
+
+    data = await state.get_data()
+    lang = data.get("language", "english")
     await state.update_data(chat_memory=None)
-    await message.answer("***⚙️ Commands:***\n\n/new - Start new conversation.\n/agents - Choose an agent\n/settings - Choose a language", parse_mode="Markdown")
+
+    if lang == "english":
+        await message.answer("***⚙️ Commands:***\n\n/new - Start new conversation.\n/agents - Choose an agent\n/language - Choose a language", parse_mode="Markdown")
+    elif lang == "russian":
+        await message.answer("***⚙️ Команды:***\n\n/new - Начать новый диалог.\n/agents - Выбрать агента\n/language - Выбрать язык", parse_mode="Markdown")
 
 @dp.message_handler(Command('new'), state="*")
 async def begin_conversation(message: types.Message, state: FSMContext):
@@ -55,33 +76,56 @@ async def begin_conversation(message: types.Message, state: FSMContext):
     await StateMachine.CHAT.set()
     data = await state.get_data()
     lang = data.get("language", "english")
+    agent = data.get("agent", "Basic")
 
-    message = await message.answer("⚡️ Starting new conversation...")
+    if lang == "english":
+        message__ = await message.answer("⚡️ Starting new conversation...")
+    elif lang == "russian":
+        message__ = await message.answer("⚡️ Начинаю новый диалог...")
 
     prompt = ChatPromptTemplate.from_messages([
         SystemMessagePromptTemplate.from_template(
             SYSTEM_PROMPT
         ),
         SystemMessagePromptTemplate.from_template(
-            f"Your customer name is {message.from_user.first_name}. Generate first message in {lang}."
+            INIT_MSG
+        ),
+        SystemMessagePromptTemplate.from_template(
+            f"Your customer name is {message.from_user.first_name}. Initiate conversation in {lang}. Remember that you need to sell a product to the customer."
         ),
         MessagesPlaceholder(variable_name="history"),
         HumanMessagePromptTemplate.from_template("{input}")
     ])
 
-    llm = ChatOpenAI(temperature=0, model=MODEL)
+    print(prompt)
+
+    if agent == "Basic":
+        llm = ChatOpenAI(temperature=0.5, model="gpt-3.5-turbo-16k")
+    elif agent == "Advanced":
+        llm = ChatOpenAI(temperature=0.3, model="gpt-4")
+
     memory = ConversationBufferMemory(return_messages=True)
     conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm)
-    response = conversation.predict(input="Hi")
+    response = conversation.predict(input="")
+    response = response.replace("M:", "")
 
     await state.update_data(chat_memory=memory)
 
     # update message with new response
-    await bot.edit_message_text(response, message.chat.id, message.message_id)
+    await bot.edit_message_text(response, message__.chat.id, message__.message_id)
 
 
 @dp.message_handler(lambda message: message.text, state=StateMachine.CHAT)
 async def conversation_handler(message: types.Message, state: FSMContext):
+
+    data = await state.get_data()
+    lang = data.get("language", "english")
+    agent = data.get("agent", "Basic")
+
+    if lang == "english":
+        message__ = await message.answer("🌀 Bot is typing...")
+    elif lang == "russian":
+        message__ = await message.answer("🌀 Бот печатает...")
 
     prompt = ChatPromptTemplate.from_messages([
         SystemMessagePromptTemplate.from_template(
@@ -94,7 +138,12 @@ async def conversation_handler(message: types.Message, state: FSMContext):
         HumanMessagePromptTemplate.from_template("{input}")
     ])
 
-    llm = ChatOpenAI(temperature=0, model=MODEL)
+    print(prompt)
+
+    if agent == "Basic":
+        llm = ChatOpenAI(temperature=0.5, model="gpt-3.5-turbo-16k")
+    elif agent == "Advanced":
+        llm = ChatOpenAI(temperature=0.3, model="gpt-4")
 
     # get state data
     data = await state.get_data()
@@ -108,17 +157,43 @@ async def conversation_handler(message: types.Message, state: FSMContext):
 
     conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm)
     response = conversation.predict(input=message.text)
+    response = response.replace("M:", "")
 
     await state.update_data(chat_memory=memory)
-    await message.answer(response)
+    await bot.edit_message_text(response, message__.chat.id, message__.message_id)
 
 @dp.callback_query_handler(lambda c: c.data in ["russian", "english"], state="*")
 async def process_callback(callback_query: types.CallbackQuery, state: FSMContext):
     lang = callback_query.data
     await state.update_data(language=lang)
 
+    message = callback_query.message
+
     await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, f"Language set to {lang.upper()}")
+    if lang == "english":
+        await bot.edit_message_text("🇺🇸 Language set to English", message.chat.id, message.message_id)
+    elif lang == "russian":
+        await bot.edit_message_text("🇷🇺 Язык изменен на русский", message.chat.id, message.message_id)
+       
+
+
+@dp.callback_query_handler(lambda c: c.data in ["Basic", "Advanced"], state="*")
+async def process_callback_agents(callback_query: types.CallbackQuery, state: FSMContext):
+    agent = callback_query.data
+    await state.update_data(agent=agent)
+
+    data = await state.get_data()
+    lang = data.get("language", "english")
+
+    message = callback_query.message
+
+    await bot.answer_callback_query(callback_query.id)
+
+    if lang == "english":
+        await bot.edit_message_text(f"🤖 Agent set to {agent}", message.chat.id, message.message_id)
+    elif lang == "russian":
+        await bot.edit_message_text(f"🤖 Агент изменен на {agent}", message.chat.id, message.message_id)
+        
 
 if __name__ == '__main__':
     from aiogram import executor
--- prompts.py
+++ prompts.py
@@ -1,1 +1,17 @@
 SYSTEM_PROMPT = """You are sales manager named Андрей. Your task is to sell a product to a customer. You are having a conversation in telegram. You are selling the course 'Арбитраж криптовалют' for 2599 RUB. You will be given a knowledge base in order to improve your responses."""
+
+
+INIT_MSG = """Example conversation:
+M: Good afternoon! My name is [Manager's Name], and I present to you our innovative product - an infocourse on cryptocurrency algotrading and arbitrage. This course will help you learn how to make money on high-yield arbitrage bundles and profit up to 3-6 percent per day. Are you interested?
+K: This sounds interesting, but it seems to me that this kind of income may not be available to ordinary people. And how much capital do you need to get started?
+M: I understand your doubts, but our infocourse is designed so that anyone can learn algorithmic trading and arbitrage techniques, even with a small initial capital. In fact, with as little as $2,000 you can start making money.
+K: That sounds promising. But what are arbitrage bundles and how can they generate income?
+M: Arbitrage bundles are a strategy that allows you to profit from price differences in cryptocurrencies on different exchanges. The idea is to buy a cryptocurrency on one exchange at a lower price and sell it on another exchange at a higher price. The difference between the purchase and sale price becomes your profit.
+K: And how much can you earn from such arbitrage bundles?
+M: The earnings depend on the volatility of the market and your ability to choose the right bundles. With our infocourse we will teach you how to analyze the market, determine promising connections and execute successful operations. Statistics show that experienced traders can earn up to 3-6 percent profit per day on arbitrage picks.
+K: That sounds attractive. What other benefits will I gain by choosing your info course?
+M: In addition to detailed algotrading and arbitrage training, we give you access to our exclusive knowledge base where you will find up-to-date analytics, strategies, and decision-making signals. You'll also get the support of our experts and the opportunity to network with other community members. We strive to provide you with all the tools you need for successful trading and financial independence.
+K: Thank you for your information. I am interested in your info course. How can I purchase it?
+M: I am glad you are interested in our info course! To purchase the course, just leave your contact information and I'll send you detailed information about the cost and purchasing process. You can also ask me any additional questions and I'll be happy to help you.
+K: Great, I'll send you my contact information. I look forward to your email.
+M: Thank you for your interest. I will get back to you as soon as possible and give you all the information you need for your successful training and trading. If you have any further questions, feel free to contact me. Good luck in achieving your financial goals!"""
