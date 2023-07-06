9ca7496ca2e0c4b504422adaa1c3c3cb1848085b
_kernelpanic <kernel@pan.ic>
2023-07-06 06:29:33 +0300

    fix

--- bot.py
+++ bot.py
@@ -121,9 +121,9 @@ async def begin_conversation(message: types.Message, state: FSMContext):
     tokens = LLAMA_GLOBAL.tokenize(SYSTEM_PROMPT.encode("utf-8"))
 
     if sampling == "top_k":
-        kwargs = {"prompt" : SYSTEM_PROMPT, "top_k" : 40, "top_p" : 0.95, "temperature" : 0.4, "repeat_penalty" : 1.1}
+        kwargs = {"prompt" : SYSTEM_PROMPT, "top_k" : 40, "top_p" : 0.95, "temperature" : 0.4, "repeat_penalty" : 1.1, "stream" : True}
     else:
-        kwargs = {"prompt" : SYSTEM_PROMPT, "mirostat_mode" : 2, "temp" : 0.4}
+        kwargs = {"prompt" : SYSTEM_PROMPT, "mirostat_mode" : 2, "temp" : 0.4, "stream" : True}
 
     print("SAMPLING: ", kwargs)
 
