5c66d717a57d2a41637aae8184a48a7e9eae840e
_kernelpanic <kernel@pan.ic>
2023-07-12 15:37:14 +0300

    uodate

--- embeddings.py
+++ embeddings.py
@@ -8,60 +8,117 @@ embeddings = OpenAIEmbeddings()
 docs = [
     Document(
         page_content=DOC_1,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_2,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_3,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_4,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_5,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_6,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_7,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_8,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_9,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_10,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_11,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_12,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_13,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_14,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_15,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_16,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_17,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_18,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
     Document(
         page_content=DOC_19,
+        metadata={
+            'title': 'The Great Gatsby',
+        }
     ),
 ]
 
