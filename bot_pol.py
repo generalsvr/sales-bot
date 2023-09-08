e7d18c8e87bf1c224cc25028b86af4b9dac00edd
GENERAL <general@gmail.com>
2023-09-08 18:19:37 +0300

    fix

--- bot_pol.py
+++ bot_pol.py
@@ -78,7 +78,7 @@ async def new_command(message: types.Message, state: FSMContext):
         message__ = await bot.send_message(message.chat.id, "📝 Ganjar sedang mengetik...")
 
     if lang == "english":
-        prompt = SYSTEM_PROMPT + memory + message.text + "\nGanjar:"
+        prompt = SYSTEM_PROMPT_EN + memory + message.text + "\nGanjar:"
     elif lang == "indonesian":
         prompt = SYSTEM_PROMPT_ID + memory + message.text + "\nGanjar:"
 
